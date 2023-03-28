66a09c36d900d743cdd80a910b1621a5e52306a1
coldstar1993 <coldstar1993@163.com>
2023-03-28 22:46:51 +0800

    improve utils

--- src/Membership.test0.ts
+++ src/Membership.test0.ts
@@ -1,188 +1,170 @@
-import { Account, AccountUpdate, fetchAccount, Field, isReady, MerkleMap, Mina, Poseidon, PrivateKey, PublicKey, shutdown, Types, UInt64 } from 'snarkyjs';
+import { AccountUpdate, fetchAccount, fetchLastBlock, Field, isReady, MerkleMap, Mina, Poseidon, PrivateKey, PublicKey, shutdown, Types } from 'snarkyjs';
 import { Membership } from './Membership.js';
-import { getProfiler } from "./profiler.js";
+import { loopUntilAccountExists, makeAndSendTransaction } from './utils.js';
 
 describe('test fuctions inside Membership', () => {
-    async function runTests(deployToBerkeley = false) {
-        let Blockchain: any;
-        let deployerAccount: PublicKey,
-            deployerKey: PrivateKey,
-            senderAccount: PublicKey,
-            senderKey: PrivateKey,
-            zkAppVerificationKey: any,
-            zkAppAddress: PublicKey,
-            zkAppPrivateKey: PrivateKey,
-            zkApp: Membership,
-            membershipMerkleMap: MerkleMap;
-        let MembershipTestProfiler = getProfiler('Membership zkApp');
+    let Blockchain: any;
+    let transactionFee = 100_000_000;
+    let senderAccount: PublicKey,
+        senderKey: PrivateKey,
+        zkAppVerificationKey: any,
+        zkAppAddress: PublicKey,
+        zkAppPrivateKey: PrivateKey,
+        zkApp: Membership,
+        membershipMerkleMap: MerkleMap;
 
-        beforeAll(async () => {
-            MembershipTestProfiler.start('isReady');
-            await isReady;
-            MembershipTestProfiler.stop();
 
-            MembershipTestProfiler.start('Membership.compile');
-            zkAppVerificationKey = (await Membership.compile()).verificationKey;
-            MembershipTestProfiler.stop();
+    let senderAcctInfo: Types.Account | undefined;
 
-            Blockchain = deployToBerkeley ? Mina.Network('https://proxy.berkeley.minaexplorer.com/graphql') : Mina.LocalBlockchain({ proofsEnabled: true });
-            Mina.setActiveInstance(Blockchain);
-        });
-
-        afterAll(() => {
-            setInterval(shutdown, 0);
-        });
-
-        beforeEach(async () => {
-            if (deployToBerkeley) {// Berkeley
-                deployerKey = PrivateKey.fromBase58('EKEvpZnLn2nMtnpwkfJJ3sLVCKxZFJguBLEhsr4PLrX4Qx7xhgAm');
-                deployerAccount = deployerKey.toPublicKey();
-
-                console.log('to fetchAccount for deployerAccount...');
-                let deployerAccountInfo: any;
-                try {
-                    deployerAccountInfo = await fetchAccount({ publicKey: deployerAccount });
-                } catch (error) {
-                    console.error(error);
-                }
-                try {
-                    if (deployerAccountInfo == undefined) {
-                        MembershipTestProfiler.start('Mina.faucet(deployerAccount)');
-                        await Mina.faucet(deployerAccount);
-                        MembershipTestProfiler.stop();
-                    }
-                } catch (error) {
-                    console.error(error);
-                }              
-                console.log('done!');
-
-                senderKey = PrivateKey.fromBase58('EKEYaNo9HxkMpNrjuAYCXug42TJnij36MkEnDsx58TfF3cFNff3n');
-                senderAccount = senderKey.toPublicKey();
-
-                console.log('to fetchAccount for senderAccount...');
-                let senderAccountInfo: any;
-                try {
-                    senderAccountInfo = await fetchAccount({ publicKey: deployerAccount });
-                } catch (error) {
-                    console.error(error);
-                }
-                try {
-                    if (senderAccountInfo == undefined) {
-                        MembershipTestProfiler.start('Mina.faucet(senderAccount)');
-                        await Mina.faucet(senderAccount);
-                        MembershipTestProfiler.stop();
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-                console.log('done!');
-
-            } else {// Local
-                ({ privateKey: deployerKey, publicKey: deployerAccount } = Blockchain.testAccounts[0]);
-                ({ privateKey: senderKey, publicKey: senderAccount } = Blockchain.testAccounts[1]);
-            }
-
-            console.log(`deployerKey: ${deployerKey.toBase58()}, deployerPubKey: ${deployerAccount.toBase58()}`);
-            console.log('initially deployerAccount.balance: ', Mina.getAccount(deployerAccount).balance.div(1e9).toString());
-            console.log(`senderKey: ${senderKey.toBase58()}, senderAccount: ${senderAccount.toBase58()}`);
-            console.log('initially senderAccount.balance: ', Mina.getAccount(senderAccount).balance.div(1e9).toString());
-
-            zkAppPrivateKey = PrivateKey.random();
-            zkAppAddress = zkAppPrivateKey.toPublicKey();
-            zkApp = new Membership(zkAppAddress);
-            console.log('zkAppAddress: ', zkAppAddress.toBase58());
-
-            let needDeploy = false;
-            if (deployToBerkeley) {// Berkeley
-                MembershipTestProfiler.start('Mina.fetchAccount(zkAppAddress)');
-                let zkAppAccountInfo: any;
-                try {
-                    zkAppAccountInfo = await fetchAccount({ publicKey: zkAppAddress });
-                } catch (error) {
-                    console.error(error);
-                }
-                MembershipTestProfiler.stop();
-                if (zkAppAccountInfo == undefined || zkAppAccountInfo.account == undefined) {
-                    // check if need to deploy to Berkeley Network
-                    needDeploy = true;
-                }
-            }
-
-            if (!deployToBerkeley || needDeploy) {
-                // deploy zkApp
-                let tx = await Mina.transaction(deployerAccount, () => {
-                    AccountUpdate.fundNewAccount(deployerAccount);
-                    zkApp.deploy({ zkappKey: zkAppPrivateKey, verificationKey: zkAppVerificationKey });
-                });
-                let txId = await tx.sign([deployerKey]).send();
-                console.log(`deployment tx send-0: txId.isSuccess:`, txId.isSuccess);
-                await txId.wait();
-                console.log(`deployment tx send-1: txId.isSuccess:`, txId.isSuccess);
-            }
-
-            membershipMerkleMap = new MerkleMap();
-            const merkleRoot0 = membershipMerkleMap.getRoot();
-            console.log(`membershipMerkleMap's initial root: ${merkleRoot0.toString()}`);
-        });
-
-        it(`store an existing user`, async () => {
-            let oneUserPriKey = PrivateKey.random();
-            let oneUserPubKey = oneUserPriKey.toPublicKey();
-            console.log(`oneUserPriKey: ${oneUserPriKey.toBase58()},  oneUserPubKey: ${oneUserPubKey.toBase58()}`)
-            let indx = Poseidon.hash(oneUserPubKey.toFields());
-            // store the user
-            membershipMerkleMap.set(indx, Field(1));
-            // get witness for existence
-            let oneUserMerkleMapWitness = membershipMerkleMap.getWitness(indx);
-            console.log(`oneUserMerkleMapWitness: ${oneUserMerkleMapWitness}`)
-            // construct a tx and send
-            MembershipTestProfiler.start('Mina.transaction()');
-            const tx = await Mina.transaction(senderAccount, () => {
-                zkApp.addNewMember(oneUserPubKey, oneUserMerkleMapWitness);
-            });
-            MembershipTestProfiler.stop();
-            MembershipTestProfiler.start('tx.prove()');
-            await tx.prove();
-            MembershipTestProfiler.stop();
-            tx.sign([senderKey]);
-            MembershipTestProfiler.start('tx.send()');
-            await tx.send();
-            MembershipTestProfiler.stop();
-        });
-
-        it(`store an non-existing user`, async () => {
-            let oneUserPriKey = PrivateKey.random();
-            let oneUserPubKey = oneUserPriKey.toPublicKey();
-            console.log(`oneUserPriKey: ${oneUserPriKey.toBase58()},  oneUserPubKey: ${oneUserPubKey.toBase58()}`)
-
-            // get witness for existence
-            let indx = Poseidon.hash(oneUserPubKey.toFields());
-            let oneUserValue = membershipMerkleMap.get(indx);
-            console.log(`oneUserValue: ${oneUserValue.toString()}`);
-            let oneUserMerkleMapWitness = membershipMerkleMap.getWitness(indx);
-            console.log(`oneUserMerkleMapWitness: ${oneUserMerkleMapWitness.toJSON()}`)
-
-            // construct a tx and send
-            MembershipTestProfiler.start('Mina.transaction()');
-            const tx = await Mina.transaction(senderAccount, () => {
-                zkApp.addNewMember(oneUserPubKey, oneUserMerkleMapWitness);
-            });
-            MembershipTestProfiler.stop();
-
-            MembershipTestProfiler.start('tx.prove()');
-            await tx.prove();
-            MembershipTestProfiler.stop();
-
-            tx.sign([senderKey]);
-            MembershipTestProfiler.start('tx.send()');
-            await tx.send();
-            MembershipTestProfiler.stop().store();
-
-            // store the user
-            // membershipMerkleMap.set(indx, Field(1));
-        });
+    async function syncNetworkStatus() {
+        if (process.env.TEST_ON_BERKELEY! == 'true') {
+            await fetchLastBlock();
+            console.log('sync Berkeley Network status: done!');
+        }
+        console.log('current network state: ', JSON.stringify(Mina.activeInstance.getNetworkState()));
+        return Mina.activeInstance.getNetworkState();
     }
 
-    //  runTests(false);
-    runTests(true);
+    async function syncAcctInfo(acctAddr: PublicKey) {
+        let acctInfo: Types.Account | undefined;
+        if (process.env.TEST_ON_BERKELEY! == 'true') {
+            acctInfo = (await fetchAccount({ publicKey: acctAddr })).account!;
+        } else {
+            acctInfo = Mina.activeInstance.getAccount(acctAddr);
+        }
+
+        return acctInfo;
+    }
+
+    beforeAll(async () => {
+        await isReady;
+
+        zkAppVerificationKey = (await Membership.compile()).verificationKey;
+
+        Blockchain = process.env.TEST_ON_BERKELEY! == 'true' ? Mina.Network({
+            mina: 'https://proxy.berkeley.minaexplorer.com/graphql',
+            archive: 'https://archive.berkeley.minaexplorer.com/',
+        }) : Mina.LocalBlockchain({ proofsEnabled: true });
+        Mina.setActiveInstance(Blockchain);
+    });
+
+    afterAll(() => {
+        setInterval(shutdown, 0);
+    });
+
+    beforeEach(async () => {
+        await syncNetworkStatus();
+
+        if (process.env.TEST_ON_BERKELEY! == 'true') {// Berkeley
+            senderKey = PrivateKey.fromBase58('EKDmWEWjC6UampzAph9ddAbnmuBgHAfiQhAmSVT6ACJgPFzCsoTW');
+            senderAccount = senderKey.toPublicKey();//    pubKey:  B62qkvenQ4bZ5qt5QJN8bmEq92KskKH4AZP7pgbMoyiMAccWTWjHRoD
+
+            console.log(`Funding fee payer ${senderAccount.toBase58()} and waiting for inclusion in a block..`);
+            // await Mina.faucet(senderAccount);
+            await loopUntilAccountExists({
+                address: senderAccount,
+                eachTimeNotExist: () => { console.log('[loopUntilAccountExists] senderAccount is still not exiting, loop&wait...'); },
+                isZkAppAccount: false,
+                isLocalBlockChain: false
+            });
+            console.log('senderAccount is funded!');
+
+        } else {// Local
+            ({ privateKey: senderKey, publicKey: senderAccount } = Blockchain.testAccounts[0]);
+        }
+
+        console.log(`senderKey: ${senderKey.toBase58()}, senderAccount: ${senderAccount.toBase58()}`);
+        senderAcctInfo = await syncAcctInfo(senderAccount);
+        let { nonce, balance } = senderAcctInfo;
+        console.log(`initially, senderAccount.nonce: ${nonce}, senderAccount.balance: ${balance}`);
+
+        zkAppPrivateKey = PrivateKey.random();
+        zkAppAddress = zkAppPrivateKey.toPublicKey();
+        zkApp = new Membership(zkAppAddress);
+        console.log('zkAppAddress: ', zkAppAddress.toBase58());
+
+        // deploy zkApp
+        console.log(`Membership Contract: deploying...`);
+        let tx_deployMembership = await Mina.transaction({ sender: senderAccount, fee: transactionFee }, () => {
+            AccountUpdate.fundNewAccount(senderAccount);
+            zkApp.deploy({ zkappKey: zkAppPrivateKey, verificationKey: zkAppVerificationKey });
+        });
+        let txId_deployMembership = await tx_deployMembership.sign([senderKey]).send();
+        console.log(`Membership Contract: deployment tx[${txId_deployMembership.hash()!}] sent...`);
+        await txId_deployMembership.wait({ maxAttempts: 1000 });
+        console.log(`Membership Contract: txId.isSuccess:`, txId_deployMembership.isSuccess);
+
+        membershipMerkleMap = new MerkleMap();
+        const merkleRoot0 = membershipMerkleMap.getRoot();
+        console.log(`membershipMerkleMap's initial root: ${merkleRoot0.toString()}`);
+    });
+
+    it(`CHECK tx should succeed when store an non-existing user, AND fail when store an existing user`, async () => {
+        console.log('===================[CHECK tx should succeed when store an non-existing user]===================');
+
+        let oneUserPriKey = PrivateKey.random();
+        let oneUserPubKey = oneUserPriKey.toPublicKey();
+        console.log(`oneUserPriKey: ${oneUserPriKey.toBase58()},  oneUserPubKey: ${oneUserPubKey.toBase58()}`)
+
+        // get witness for existence
+        let indx = Poseidon.hash(oneUserPubKey.toFields());
+        let oneUserValue = membershipMerkleMap.get(indx);
+        console.log(`oneUserValue: ${oneUserValue.toString()}`);
+        let oneUserMerkleMapWitness = membershipMerkleMap.getWitness(indx);
+
+        // construct a tx and send
+        await makeAndSendTransaction({
+            feePayerPublicKey: senderKey.toPublicKey(),
+            zkAppAddress,
+            mutateZkApp() {
+                zkApp.addNewMember(oneUserPubKey, oneUserMerkleMapWitness);
+            },
+            transactionFee,
+            signTx(tx: Mina.Transaction) {
+                tx.sign([senderKey]);
+            },
+            getState() {
+                return zkApp.memberCount.get();
+            },
+            statesEqual(state1, state2) {
+                return state2.equals(state1).toBoolean();
+            },
+        });
+        // store the user
+        membershipMerkleMap.set(indx, Field(1));
+        expect(zkApp.memberTreeRoot).toEqual(membershipMerkleMap.getRoot());
+
+        console.log('===================As Expected, tx succeed when store an non-existing user===================');
+
+
+        console.log('===================[CHECK tx should fail when store an existing user]===================');
+        // get witness for existence
+        oneUserMerkleMapWitness = membershipMerkleMap.getWitness(indx);
+        let merkleRoot0 = membershipMerkleMap.getRoot();
+        try {
+            // construct a tx and send
+            await makeAndSendTransaction({
+                feePayerPublicKey: senderKey.toPublicKey(),
+                zkAppAddress,
+                mutateZkApp() {
+                    zkApp.addNewMember(oneUserPubKey, oneUserMerkleMapWitness);
+                },
+                transactionFee,
+                signTx(tx: Mina.Transaction) {
+                    tx.sign([senderKey]);
+                },
+                getState() {
+                    return zkApp.memberCount.get();
+                },
+                statesEqual(state1, state2) {
+                    return state2.equals(state1).toBoolean();
+                },
+            });
+        } catch (error) {
+            console.log('===================As Expected, tx fails when store an existing user===================');
+            console.error(error);
+        }
+        // store the user
+        expect(zkApp.memberTreeRoot).toEqual(merkleRoot0);
+    });
 });
