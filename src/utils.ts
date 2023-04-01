ae7db16f63a36fa0d70bfe336959849cb93d1dc4
coldstar1993 <coldstar1993@163.com>
2023-04-01 22:38:44 +0800

    add syncActions as a util

--- src/utils.ts
+++ src/utils.ts
@@ -6,13 +6,40 @@ import {
   fetchAccount,
   Types,
   fetchLastBlock,
-  UInt32
+  UInt32,
 } from 'snarkyjs';
+import { fetchActions } from 'snarkyjs/dist/node/lib/fetch.js';
+
+export async function syncActions(targetAddr: PublicKey, isLocalBlockChain?: boolean) {
+  if (!isLocalBlockChain) {
+    for (let i = 0; i < 5; i++) {// just for 5 iterations for 5 blocks, enough
+      let actionsList;
+      try {
+        // get the length of actions list, and compare later to confirm the tx is done!
+        actionsList = await fetchActions({ publicKey: targetAddr.toBase58() });// will throw error if duplicate actions issue.
+      } catch (error) {// exisitng issue: duplicate actions 
+        console.log(`error: await fetchActions({ publicKey: ${targetAddr.toBase58()} }): `, JSON.stringify(error));
+
+        console.log(`wait for a block and fetchActions again...`);
+        await waitBlockHeightToExceed((await syncNetworkStatus()).blockchainLength.add(1));
+      }
+
+      if (!(actionsList instanceof Array)) {
+        console.log(`error: await fetchActions({ publicKey: ${targetAddr.toBase58()} }): `, JSON.stringify(actionsList));
+        throw new Error('fetchActions failed! Pls try later.');
+      }
+
+      return actionsList;
+    }
+  } else {
+    return Mina.activeInstance.getActions(targetAddr);
+  }
+}
 
 export async function syncNetworkStatus(isLocalBlockChain?: boolean) {
   if (!isLocalBlockChain) {
-      await fetchLastBlock();
-      console.log('sync Berkeley Network status: done!');
+    await fetchLastBlock();
+    console.log('sync Berkeley Network status: done!');
   }
   console.log('current network state: ', JSON.stringify(Mina.activeInstance.getNetworkState()));
   return Mina.activeInstance.getNetworkState();
@@ -20,22 +47,22 @@ export async function syncNetworkStatus(isLocalBlockChain?: boolean) {
 
 export async function waitBlockHeightToExceed(aHeight: UInt32, isLocalBlockChain?: boolean) {
   if (!isLocalBlockChain) {
-      // wait for Berkeley's blockchainLength > aHeight
-      while (true) {
-          let blockchainLength = (await syncNetworkStatus()).blockchainLength;
-          console.log(`aHeight: ${aHeight.toString()}, current blockchainLength: ${blockchainLength.toString()}`);
+    // wait for Berkeley's blockchainLength > aHeight
+    while (true) {
+      let blockchainLength = (await syncNetworkStatus()).blockchainLength;
+      console.log(`aHeight: ${aHeight.toString()}, current blockchainLength: ${blockchainLength.toString()}`);
 
-          if (aHeight.lessThan(blockchainLength).toBoolean()) {
-              break;
-          }
-
-          let blockGap = Number.parseInt(aHeight.sub(blockchainLength).toString());
-          blockGap = blockGap == 0 ? 1 : blockGap;
-          await new Promise((resolve) => setTimeout(resolve, blockGap * 3 * 60 * 1000));// about 3 minutes/block
+      if (aHeight.lessThan(blockchainLength).toBoolean()) {
+        break;
       }
+
+      let blockGap = Number.parseInt(aHeight.sub(blockchainLength).toString());
+      blockGap = blockGap == 0 ? 1 : blockGap;
+      await new Promise((resolve) => setTimeout(resolve, blockGap * 3 * 60 * 1000));// about 3 minutes/block
+    }
   } else {
-      (Mina.activeInstance as any).setBlockchainLength(aHeight.add(1));
-      console.log(`aHeight: ${aHeight.toString()}, current blockchainLength: ${Mina.activeInstance.getNetworkState().blockchainLength.toString()}`);
+    (Mina.activeInstance as any).setBlockchainLength(aHeight.add(1));
+    console.log(`aHeight: ${aHeight.toString()}, current blockchainLength: ${Mina.activeInstance.getNetworkState().blockchainLength.toString()}`);
   }
   console.log('current network state: ', JSON.stringify(Mina.activeInstance.getNetworkState()));
 }
@@ -160,7 +187,9 @@ export const makeAndSendTransaction = async <State extends ToString>({
       'waiting for zkApp state to change... (current state: ',
       state.toString() + ')'
     );
-    await new Promise((resolve) => setTimeout(resolve, 1 * 60 * 1000));
+    if (!isLocalBlockChain) {
+      await new Promise((resolve) => setTimeout(resolve, 1 * 60 * 1000));
+    }
     await syncAcctInfo(zkAppAddress, tokenId, isLocalBlockChain);
     state = await getState();
     stateChanged = !statesEqual(initialState, state);
