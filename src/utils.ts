e468fbc4bd4b34f41d82073cdf979f8c81a1a98b
coldstar1993 <coldstar1993@163.com>
2023-04-01 14:11:47 +0800

    imprv utils

--- src/utils.ts
+++ src/utils.ts
@@ -5,23 +5,60 @@ import {
   PublicKey,
   fetchAccount,
   Types,
+  fetchLastBlock,
+  UInt32
 } from 'snarkyjs';
 
-async function syncAcctInfo(acctAddr: PublicKey, isLocalBlockChain?: boolean) {
+export async function syncNetworkStatus(isLocalBlockChain?: boolean) {
+  if (!isLocalBlockChain) {
+      await fetchLastBlock();
+      console.log('sync Berkeley Network status: done!');
+  }
+  console.log('current network state: ', JSON.stringify(Mina.activeInstance.getNetworkState()));
+  return Mina.activeInstance.getNetworkState();
+}
+
+export async function waitBlockHeightToExceed(aHeight: UInt32, isLocalBlockChain?: boolean) {
+  if (!isLocalBlockChain) {
+      // wait for Berkeley's blockchainLength > aHeight
+      while (true) {
+          let blockchainLength = (await syncNetworkStatus()).blockchainLength;
+          console.log(`aHeight: ${aHeight.toString()}, current blockchainLength: ${blockchainLength.toString()}`);
+
+          if (aHeight.lessThan(blockchainLength).toBoolean()) {
+              break;
+          }
+
+          let blockGap = Number.parseInt(aHeight.sub(blockchainLength).toString());
+          blockGap = blockGap == 0 ? 1 : blockGap;
+          await new Promise((resolve) => setTimeout(resolve, blockGap * 3 * 60 * 1000));// about 3 minutes/block
+      }
+  } else {
+      (Mina.activeInstance as any).setBlockchainLength(aHeight.add(1));
+      console.log(`aHeight: ${aHeight.toString()}, current blockchainLength: ${Mina.activeInstance.getNetworkState().blockchainLength.toString()}`);
+  }
+  console.log('current network state: ', JSON.stringify(Mina.activeInstance.getNetworkState()));
+}
+
+
+export async function syncAcctInfo(acctAddr: PublicKey, tokenId?: Field, isLocalBlockChain?: boolean) {
   let acctInfo: Types.Account | undefined;
+  if (typeof tokenId == 'undefined') {
+    tokenId = Field(1);
+  }
   if (isLocalBlockChain) {
     console.log('isLocalBlockChain: ', isLocalBlockChain);
-    acctInfo = Mina.activeInstance.getAccount(acctAddr);
+    acctInfo = Mina.activeInstance.getAccount(acctAddr, tokenId);
   } else {
-    acctInfo = (await fetchAccount({ publicKey: acctAddr })).account!;
+    acctInfo = (await fetchAccount({ publicKey: acctAddr, tokenId })).account!;
   }
   return acctInfo;
 }
 
 // ========================================================
 
-export const checkAccountExists = async (address: PublicKey, isLocalBlockChain?: boolean) => {
-  let account = await syncAcctInfo(address, isLocalBlockChain);
+export const checkAccountExists = async (address: PublicKey, tokenId?: Field, isLocalBlockChain?: boolean) => {
+  let account = await syncAcctInfo(address, tokenId, isLocalBlockChain);
   let accountExists = account != undefined && account != null;
   return { accountExists, account };
 }
@@ -30,19 +67,21 @@ export const checkAccountExists = async (address: PublicKey, isLocalBlockChain?:
 
 export const loopUntilAccountExists = async (
   { address,
+    tokenId,
     eachTimeNotExist,
     isZkAppAccount,
     isLocalBlockChain
   }:
     {
       address: PublicKey,
+      tokenId?: Field,
       eachTimeNotExist: () => void,
       isZkAppAccount: boolean,
       isLocalBlockChain?: boolean
     }
 ) => {
   for (; ;) {
-    let { accountExists, account } = await checkAccountExists(address, isLocalBlockChain);
+    let { accountExists, account } = await checkAccountExists(address, tokenId, isLocalBlockChain);
     console.log(`[checkAccountExists]: ${accountExists}, account: ${JSON.stringify(account)}`);
     if (accountExists && isZkAppAccount) {
       accountExists = accountExists && (account as any)?.zkapp.appState != null;
@@ -69,6 +108,7 @@ type FetchedAccount = NonNullable<FetchedAccountResponse["account"]>
 export const makeAndSendTransaction = async <State extends ToString>({
   feePayerPublicKey,
   zkAppAddress,
+  tokenId,
   mutateZkApp,
   transactionFee,
   signTx,
@@ -78,6 +118,7 @@ export const makeAndSendTransaction = async <State extends ToString>({
 }: {
   feePayerPublicKey: PublicKey,
   zkAppAddress: PublicKey,
+  tokenId?: Field,
   mutateZkApp: () => void,
   transactionFee: number,
   signTx: (tx: Mina.Transaction) => void,
@@ -87,7 +128,7 @@ export const makeAndSendTransaction = async <State extends ToString>({
 }) => {
   // Why this line? It increments internal feePayer account variables, such as
   // nonce, necessary for successfully sending a transaction
-  await syncAcctInfo(zkAppAddress, isLocalBlockChain);
+  await syncAcctInfo(zkAppAddress, tokenId, isLocalBlockChain);
   const initialState = await getState();
 
   let transaction = await Mina.transaction(
@@ -120,7 +161,7 @@ export const makeAndSendTransaction = async <State extends ToString>({
       state.toString() + ')'
     );
     await new Promise((resolve) => setTimeout(resolve, 1 * 60 * 1000));
-    await syncAcctInfo(zkAppAddress, isLocalBlockChain);
+    await syncAcctInfo(zkAppAddress, tokenId, isLocalBlockChain);
     state = await getState();
     stateChanged = !statesEqual(initialState, state);
     console.log('==== stateChanged: ', stateChanged);
